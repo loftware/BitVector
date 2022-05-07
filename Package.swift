1ba4926603172595baf4eb9ebef47759973f2e8d
Dave Abrahams <dabrahams@adobe.com>
2022-05-07 12:16:21 -0700

    Work

--- Package.swift
+++ Package.swift
@@ -1,28 +1,37 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.5
 import PackageDescription
 
+let auxilliaryFiles = ["README.md", "LICENSE"]
 let package = Package(
-    name: "BitVector",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "BitVector",
-            targets: ["BitVector"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "BitVector",
-            dependencies: []),
-        .testTarget(
-            name: "BitVectorTests",
-            dependencies: ["BitVector"]),
-    ]
+  name: "LoftDataStructures_BitVector",
+  products: [
+    .library(
+      name: "LoftDataStructures_BitVector",
+      targets: ["LoftDataStructures_BitVector"]),
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/loftware/StandardLibraryProtocolChecks",
+      from: "0.1.2"
+    ),
+  ],
+  targets: [
+    .target(
+      name: "LoftDataStructures_BitVector",
+      path: ".",
+      exclude: auxilliaryFiles + ["Tests.swift"],
+      sources: ["BitVector.swift"]),
+
+    .testTarget(
+      name: "Test",
+      dependencies: [
+        "LoftDataStructures_BitVector",
+        .product(name: "LoftTest_StandardLibraryProtocolChecks",
+                 package: "StandardLibraryProtocolChecks"),
+      ],
+      path: ".",
+      exclude: auxilliaryFiles + ["BitVector.swift"],
+      sources: ["Tests.swift"]
+    ),
+  ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import BitVectorTests
-
-var tests = [XCTestCaseEntry]()
-tests += BitVectorTests.allTests()
-XCTMain(tests)
